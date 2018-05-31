597ba38f77068bf87ed6d92f83eb19387a0c6b76
ForteMare <30961251+ForteMare@users.noreply.github.com>
2018-05-31 22:45:42 +0200

    Initiated objects in FalseDatabaseStarter for help with setting up the table.

--- src/inventory/FalseDatabaseStarter.java
+++ src/inventory/FalseDatabaseStarter.java
@@ -6,6 +6,10 @@ This class is temporary.
 
 package inventory;
 
+import inventory.multimedia.audio.Podcast;
+import inventory.multimedia.video.Movie;
+import inventory.print.book.Novel;
+
 import java.util.ArrayList;
 
 public class FalseDatabaseStarter {
@@ -14,8 +18,8 @@ public class FalseDatabaseStarter {
 
     public static void startDatabase() {
 
-        /*
-        Temporarily unavailable
-         */
+        Movie m1 = new Movie("Full Metal Jacket", "Tristar Colombia", "War Film", "Stanley Kubrick", "Not Stanley", 7.7, true);
+        Novel n1 = new Novel("Things Fall Apart", "Chinua Achebe", "Fiction", true, 1995, "DC.51C3", "English");
+        Podcast p1 = new Podcast("Escape Pod", "Daikaiju", "Science Fiction", "MP3", false, false);
     }
 }
