49bc9466c0291725ef5075b9a7d2b37f239dc06d
ForteMare <30961251+ForteMare@users.noreply.github.com>
2018-05-31 20:58:35 +0200

    Enabled addAudio method to be called. FX works accordingly.

--- src/gui/AddItemBox.java
+++ src/gui/AddItemBox.java
@@ -1379,6 +1379,99 @@ public class AddItemBox {
 
     static void addAudio() {
 
+        // Set the stage
+        primaryStage = new Stage();
+        primaryStage.setTitle("Audio");
+        primaryStage.initModality(Modality.APPLICATION_MODAL);
+
+        // Initiate Accept button
+        accept = new Button("Save");
+        accept.setMinSize(90, 30);
+        accept.setMaxSize(30, 10);
+
+        // Initiate Reject button
+        reject = new Button("Cancel");
+        reject.setMinSize(90, 30);
+        reject.setMaxSize(30, 10);
+
+        // Setting up button actions
+        accept.setOnAction(event -> System.out.println("Not set up yet"));
+        reject.setOnAction(event -> primaryStage.close());
+
+        // Instructions
+        Label instructionsLabel = new Label("Add a new audio file: ");
+
+        // Adding fields for abstract Inventory class
+
+        // Set title
+        TextField titleInput = new TextField();
+        titleInput.setPromptText("Triglav National Park Bird Noises");
+        Label titleLabel = new Label("Title: ");
+
+        // Choice box for availability
+        ChoiceBox<String> availableInput = new ChoiceBox<>();
+        availableInput.getItems().addAll("Available", "Reserved", "Unavailable");
+        availableInput.setValue("Available");
+        availableInput.setMinWidth(190);
+        Label availableLabel = new Label("Status: ");
+
+        // Adding fields for Multimedia class
+
+        // Text field for author
+        TextField publisherInput = new TextField();
+        publisherInput.setPromptText("Birget Production");
+        Label publisherLabel = new Label("Publisher: ");
+
+        // Input sub type (previously called genre)
+        ChoiceBox<String> subDefInput = new ChoiceBox<>();
+        subDefInput.getItems().addAll("Ambient Noise", "Interview", "Court Recording", "ASMR");
+        subDefInput.setValue("Ambient Noise");
+        subDefInput.setMinWidth(190);
+        Label subDefLabel = new Label("Type: ");
+
+        // Adding input for Audio class
+
+        // Input sub type (previously called genre)
+        ChoiceBox<String> fileFormatInput = new ChoiceBox<>();
+        fileFormatInput.getItems().addAll("WAV", "MP3", "WMA", "AAC", "FLAC");
+        fileFormatInput.setValue("MP3");
+        fileFormatInput.setMinWidth(190);
+        Label fileFormatLabel = new Label("File format: ");
+
+        // Setting up the Grid Pane
+        layout = new GridPane();
+        layout.setPadding(new Insets(20, 45, 45, 45));
+        layout.setHgap(70);
+        layout.setVgap(15);
+
+        // Adding Items to the Grid Pane
+
+        // First column
+        layout.add(instructionsLabel, 0, 0);
+        layout.add(titleLabel, 0, 1);
+        layout.add(titleInput, 0, 2);
+        layout.add(publisherLabel, 0, 3);
+        layout.add(publisherInput, 0, 4);
+        layout.add(subDefLabel, 0, 5);
+        layout.add(subDefInput, 0, 6);
+
+        //Second column
+        layout.add(fileFormatLabel, 1, 1);
+        layout.add(fileFormatInput, 1, 2);
+        layout.add(availableLabel, 1, 3);
+        layout.add(availableInput, 1, 4);
+
+        // Buttons
+        layout.add(accept, 0, 11);
+        layout.add(reject, 0, 12);
+
+        // Set up the scene
+        scene = new Scene(layout, 550, 510);
+
+        // Activate the primary stage
+        primaryStage.setScene(scene);
+        primaryStage.showAndWait();
+
     }
 
 }
